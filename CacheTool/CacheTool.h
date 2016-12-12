ed453c74bc8353b3d7a0f59a1e40ff8cdcdcc20f
runnerMJP <1249895792@qq.com>
2016-12-12 17:30:22 +0800

    1

---
+++ CacheTool/CacheTool.h
@@ -0,0 +1,25 @@
+//
+//  CacheTool.h
+//  BuDeJie
+//
+//  Created by MJP on 16/11/29.
+//  Copyright © 2016年 YSAN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@interface CacheTool : NSObject
+
+
+/**
+ 计算一个文件夹的大小
+ */
++(NSString *)getCacheSize:(NSString*)cachePath;
+
+
+/**
+ 删除文件夹里的所有文件
+ */
++(void)removeCache:(NSString *)cachePath;
+
+@end
